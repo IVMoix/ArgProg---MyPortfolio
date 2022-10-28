e9bfe8e1de574e49c1fb4cc05b8e1fe5129222aa
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2022-10-28 18:43:00 -0300

    Componentes
    Se crearon los componentes necesarios para le front-end del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { HardNSoftSkillsComponent } from './components/hard-n-soft-skills/hard-n-soft-skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    HardNSoftSkillsComponent,
+    AboutComponent,
+    ProyectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hard-n-soft-skills/hard-n-soft-skills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-n-soft-skills',
+  templateUrl: './hard-n-soft-skills.component.html',
+  styleUrls: ['./hard-n-soft-skills.component.css']
+})
+export class HardNSoftSkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
