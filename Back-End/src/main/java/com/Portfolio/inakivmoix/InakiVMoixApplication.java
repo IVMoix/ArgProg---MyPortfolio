e342c80c876e985e33b2ada5bd36cabb5503ab9b
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2022-10-31 15:10:59 -0300

    SpringBoot
    Se agrego a la carpeta Back-end las configuracion de Spring-Boot

---
+++ Back-End/src/main/java/com/Portfolio/inakivmoix/InakiVMoixApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.inakivmoix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InakiVMoixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InakiVMoixApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/Portfolio/inakivmoix/InakiVMoixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.inakivmoix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InakiVMoixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
